82f67b37d507a2f996a3043cd0d6c491bd429adf
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2025-07-01 11:50:54 +0100

    Adjust layout to remove scrollbar and add words array

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const words = [
+    {
+        word: "elephant",
+        clue: "The biggest land animal in the world."
+    },
+    {
+        word: "lion",
+        clue: "The king of the big cats."
+    },
+    {
+        word: "cheetah",
+        clue: "The fastest land animal in the world."
+    },
+    {
+        word: "giraffe",
+        clue: "An animal with a long neck for reaching leaves."
+    },
+    {
+        word: "penguin",
+        clue: "A flightless bird from Antarctica."
+    },
+    {
+        word: "spider",
+        clue: "A creepy-crawly with eight legs."
+    },
+    {
+        word: "dolphin",
+        clue: "A super speedy, high-jumping marine mammal."
+    },
+    {
+        word: "gorilla",
+        clue: "A big, strong forest primate."
+    },
+    {
+        word: "crocodile",
+        clue: "A scaly, snappy river reptile."
+    }
+];
+
